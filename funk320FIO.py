6b192fc4f46960bd3e6479295f77ed33303738e4
blacksamurai2006-ai <blacksamurai2006@gmail.com>
2026-03-04 12:59:59 +0500

    Delete funk320FIO.py

--- funk320FIO.py
+++
@@ -1,8 +0,0 @@
-import random
-def funk320FIO(n, k):
-    n = int(input("Введите n: "))
-    k = int(input("Введите k: "))
-    arr = []
-    for i in range(k):
-        arr.append(random.randint(1, n))
-    return arr
