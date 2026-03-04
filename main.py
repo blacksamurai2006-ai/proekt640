7ab6e5f7e2f895f80b7209a87b6db5b218b25b98
Apeperr <Adjelimovaz@gmail.com>
2026-03-04 12:43:30 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from funk320FIO import funk320FIO
-n = 10
-k = 5
-result = funk320FIO(n, k)
-print("Результат работы функции:", result)
+from proekt320Arsen import funk320Arsen
+
+def main():
+    x = int(input("Ввод x: "))
+    y = int(input("Ввод y: "))
+
+    result = funk320Arsen(x, y)
+
+    print("Тестирование функции funk320Arsen")
+    print(f"x = {x}, y = {y}")
+    print(f"Результат = {result}")
+
+if __name__ == "__main__":
+    main()
