0e71a0972a6b31661628157e9e6aa77631fae0cd
sendy-trew <sendytrew@gmail.com>
2026-03-04 12:40:54 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-import proekt640Mukumniyazov
-proekt640Mukumniyazov.funk640Mukumniyazov(0)
+from funk320FIO import funk320FIO
+n = 10
+k = 5
+result = funk320FIO(n, k)
+print("Результат работы функции:", result)
